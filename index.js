e3db57652d611486c6d262f5a9431cddd05c6357
vishu-neetu <2018026@iiitdmj.ac.in>
2021-06-01 17:36:48 +0530

    added color and mobile touch

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 const express = require('express');
 const app = express();
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-let connections = [];
+app.use(express.static(__dirname + '/public'));
 
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
 
-// connection established
-io.on('connect', (socket)=> {
-    connections.push(socket);
-    console.log(`${socket.id} has connected`);
-    // console.log(connections);
-    // event draw when connection is on
-    socket.on('draw', (data)=>{
-        connections.forEach((con) => {
-            if(con.id !== socket.id){
-                con.emit('ondraw', {x: data.x,y: data.y,c :data.c})
-            }
-        });
-    })
+io.on('connection', onConnection);
 
-    // event down when connection is on
-    socket.on('down', (data)=>{
-        connections.forEach((con)=>{
-            if(con.id !== socket.id){
-                con.emit('ondown', {x: data.x, y: data.y})
-            }
-        })
-    })
-
-    // when socket connection gets disconnected
-    socket.on('disconnect', (reason) => {
-        console.log(`${socket.id} got disconnected`)
-        // console.log(connections);
-        connections = connections.filter((con)=> con.id !== socket.id); 
-    });
-});
-
-
-
-app.use(express.static('public'));
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, ()=> console.log('server started'))
+http.listen(port, () => console.log('listening on port ' + port));
